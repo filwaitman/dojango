86c2bdf736150b52373e20e62f95d31e51be5bfe
Filipe Waitman <filwaitman@gmail.com>
2019-12-27 12:03:00 -0300

    Deal with remainder for versions like "1.5.0-jsonrpc"

--- dojango/version.py
+++ dojango/version.py
@@ -1,4 +1,4 @@
 # -*- encoding: utf-8 -*-
 
 def version_tuple(s):
-    return tuple([int(x) for x in s.split(".")])
+    return tuple([int(x) for x in s.split("-")[0].split(".")])  # Deal with remainder for versions like "1.5.0-jsonrpc"
